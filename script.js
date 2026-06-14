1ebca8e88399be27cd8707c89207f6051c043aa9
Ariel Arauz <ariel.arauz@udla.edu.ec>
2026-06-14 15:09:34 -0500

    Interfaz-Central-Nuclear

---
+++ script.js
@@ -0,0 +1,129 @@
+let temperatura=380;
+let encendido = false;
+let agua = 70;
+let velocidad = 1;
+
+const alarma = document.getElementById("alarma");
+alarma.volume = 0.05;
+let alarmaActiva = false;
+
+setInterval(() => {
+    temperatura+= velocidad;
+    temperatura = Number(temperatura.toFixed(2));
+    controlarAlarma();
+     document.getElementById("temperatura").innerText = temperatura + "°C";
+    },1000 ); 
+    setInterval(() => {
+    if (temperatura > 400) {
+        document.getElementById("temperatura").style.color = "red";
+        const overlay = document.getElementById("overlay");
+        overlay.style.opacity = "0.3";
+        setTimeout(() => {
+            overlay.style.opacity = "0";
+        },500);
+
+    } else {
+        document.getElementById("overlay").style.opacity = "0";
+        document.getElementById("temperatura").style.color = "white";
+    }
+}, 2000);
+
+
+let motorEnviar = null;
+let motorDesfogar = null;
+const btnagua = document.querySelector(".btnagua");
+const btndesfogar = document.querySelector(".btndesfogar");
+
+function actualizar() {
+    document.getElementById("temperatura").innerText =
+        Math.round(temperatura) + "°C";
+
+    document.getElementById("agua").innerText =
+        Math.floor(agua) + "%";
+    document.getElementById("barraAgua").style.width = agua + "%";
+}
+
+
+
+function iniciarEnviar() {
+    motorEnviar = setInterval(() => {
+        if (agua >= 100) {
+            agua = 100;
+            temperatura=temperatura;
+            if(temperatura<=0){
+                temperatura = 0;
+            }
+        }
+        else {
+        agua += 1;
+        temperatura -= 1.5;
+        velocidad = 0;
+        }
+
+        actualizar();
+    }, 200);
+}
+
+function pararEnviar() {
+    clearInterval(motorEnviar);
+}
+
+function desfogar() {
+    motorDesfogar = setInterval(() => {
+        if (agua <= 0) {
+            agua = 0;
+            temperatura=temperatura;
+                if(temperatura<=0){
+                    temperatura = 0;
+                }
+        }
+        else {
+        agua -= 2;
+        temperatura -= 0.25;
+        velocidad = 0;
+        }   
+        actualizar();
+    },150);
+}
+function pararDesfogar() {
+    clearInterval(motorDesfogar);
+}
+function controlarAlarma() {
+
+    if (temperatura > 400) {
+
+        if (!alarmaActiva) {
+            alarma.play();
+            alarma.loop = true;
+            alarmaActiva = true;
+        }
+
+    } else {
+
+        alarma.pause();
+        alarma.currentTime = 0;
+        alarmaActiva = false;
+    }
+}
+
+btnagua.addEventListener("mousedown", iniciarEnviar);
+btnagua.addEventListener("mouseup", pararEnviar);
+btnagua.addEventListener("mouseleave", pararEnviar);
+
+btndesfogar.addEventListener("mousedown", desfogar);
+btndesfogar.addEventListener("mouseup", pararDesfogar);
+btndesfogar.addEventListener("mouseleave", pararDesfogar);
+setInterval(()=> {
+    if(agua<30){
+    velocidad = 3;
+}
+else if(agua<50){
+    velocidad = 2.5;
+}
+else if(agua<=70){
+    velocidad = 2;
+}
+else{
+    velocidad = 1;
+}
+},1000)
